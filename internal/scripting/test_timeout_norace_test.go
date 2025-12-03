8143639e2ac5d16fc761a4d0f55ca5cc46b3964d
Joseph Cumines <joeycumines@gmail.com>
2025-12-04 06:56:33 +1000

    Add unused var ignore

--- internal/scripting/test_timeout_norace_test.go
+++ internal/scripting/test_timeout_norace_test.go
@@ -6,4 +6,6 @@ import "time"
 
 // defaultTimeout is used by tests which need to wait for interactive
 // components to complete. Non-race builds are quicker so keep it shorter.
+//
+//lint:ignore U1000 Unused depending on env.
 var defaultTimeout = 50 * time.Second
--- internal/scripting/test_timeout_race_test.go
+++ internal/scripting/test_timeout_race_test.go
@@ -6,4 +6,6 @@ import "time"
 
 // defaultTimeout is larger for race-enabled builds because the runtime may
 // slow down scheduling and IO.
+//
+//lint:ignore U1000 Unused depending on env.
 var defaultTimeout = 70 * time.Second
